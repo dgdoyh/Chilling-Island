fbe59477abb74e64fd782b86f96f9e7c6e1a4fef
dgdoyh <dgdoyh@gmail.com>
2024-12-23 19:32:19 -0500

    Initial commit

---
+++ Source/ChillingIsland/ChillingIsland.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ChillingIsland.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChillingIsland, "ChillingIsland" );
---
+++ Source/ChillingIsland/Private/Characters/PlayerCharacter.cpp
@@ -0,0 +1,100 @@
+#include "Characters/PlayerCharacter.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Disable player rotation following controller's rotation
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Rotate player to the direction of movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// Set rotation speed
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+
+	// Create a spring arm
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	// Attach it to the root component
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;
+
+	// Create and set up a view camera
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	ViewCamera->SetupAttachment(CameraBoom);
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Add a mapping context to subsystem (IMC will be set in the engine)
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(PlayerMappingContext, 0);
+		}
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+	// Get movement vector from input
+	const FVector2D MoveVector = Value.Get<FVector2D>();
+
+	// Get player controller's yaw rotation value
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw,0.f);
+
+	// Get directional vector of the controller
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	// Move player based on the controller's yaw rotation
+	AddMovementInput(ForwardDirection, MoveVector.Y);
+	AddMovementInput(RightDirection, MoveVector.X);
+	
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	// Get looking vector from input
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	// Rotate controller based on mouse axis
+	AddControllerPitchInput(LookAxisVector.Y);
+	AddControllerYawInput(LookAxisVector.X);
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Bind input actions and functions
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
+	}
+}
+
+void APlayerCharacter::Jump()
+{
+	Super::Jump();
+}
+
