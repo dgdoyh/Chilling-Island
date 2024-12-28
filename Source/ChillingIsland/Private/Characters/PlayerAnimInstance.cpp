ef7c0f08bdd09e8a0b9203c32045c3293be5eb90
dgdoyh <dgdoyh@gmail.com>
2024-12-27 20:04:24 -0500

    Re-implemented Player Animation In C++
    - Re-implemented player idle & running animation in C++

---
+++ Source/ChillingIsland/Private/Characters/PlayerAnimInstance.cpp
@@ -0,0 +1,31 @@
+#include "Characters/PlayerAnimInstance.h"
+#include "Characters/PlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Begin Play
+void UPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// Get player pawn
+	PlayerCharacter = Cast<APlayerCharacter>(TryGetPawnOwner());
+
+	// Get player's movement component
+	if (PlayerCharacter)
+	{
+		PlayerMovementComponent = PlayerCharacter->GetCharacterMovement();
+	}
+}
+
+// Tick
+void UPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (PlayerMovementComponent)
+	{
+		// Get player's current speed
+		RunningSpeed = UKismetMathLibrary::VSizeXY(PlayerMovementComponent->Velocity);
+	}
+}
